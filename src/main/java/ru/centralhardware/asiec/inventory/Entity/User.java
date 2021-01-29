f1364ffd785a2592175310b95c68bad382a0b288
Fedechkin Alexey <alex@centralhardware.ru>
2021-01-30 00:48:11 +0700

    update migration

--- src/main/java/ru/centralhardware/asiec/inventory/Entity/User.java
+++ src/main/java/ru/centralhardware/asiec/inventory/Entity/User.java
@@ -1,6 +1,10 @@
 package ru.centralhardware.asiec.inventory.Entity;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
 
 @Table(name = "inventory_users")
 @Entity
--- src/main/resources/db/migration/V1.0__initial_database.sql
+++ src/main/resources/db/migration/V1.0__initial_database.sql
@@ -1,3 +1,12 @@
-CREATE TABLE IF NOT EXISTS inventory_users (
-    id SERIAL
+CREATE TABLE inventory_users (
+    id SERIAL,
+    username VARCHAR(50),
+    password VARCHAR(64),
+    name VARCHAR(30),
+    surname VARCHAR(30),
+    last_name VARCHAR(30),
+    role VARCHAR(20),
+    is_enabled BOOLEAN,
+    is_deleted BOOLEAN,
+    last_login TIMESTAMP
 )
