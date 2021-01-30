339678f37ac79b670cdb364ee8cd51ebc2a8c1a0
Fedechkin Alexey <alex@centralhardware.ru>
2021-01-30 15:30:49 +0700

    refactor

--- src/main/java/ru/centralhardware/asiec/inventory/Entity/User.java
+++ src/main/java/ru/centralhardware/asiec/inventory/Entity/User.java
@@ -1,10 +1,6 @@
 package ru.centralhardware.asiec.inventory.Entity;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.*;
-import java.util.Collection;
 
 @Table(name = "inventory_users")
 @Entity
