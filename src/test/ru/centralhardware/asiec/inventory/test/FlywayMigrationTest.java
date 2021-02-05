547fa528a73ad7dd5def08e16a4f44be0fe33b53
aleks <aleksey-fedechkin@rambler.ru>
2021-02-05 14:25:05 +0700

    add migration test

---
+++ src/test/ru/centralhardware/asiec/inventory/test/FlywayMigrationTest.java
@@ -0,0 +1,25 @@
+package ru.centralhardware.asiec.inventory.test;
+
+import org.flywaydb.core.Flyway;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+
+public class FlywayMigrationTest {
+
+    @ClassRule
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres")
+            .withDatabaseName("integration-tests-db")
+            .withUsername("sa")
+            .withPassword("sa");
+
+    @Test
+    public void testSomethingOnYourCustomerTable() {
+        Flyway.configure()
+                .dataSource(postgreSQLContainer.getJdbcUrl(), postgreSQLContainer.getUsername(), postgreSQLContainer.getPassword())
+                .load()
+                .migrate();
+    }
+
+}
