22530772c6abf7544522a7153d3cd290cb8c6719
Fedechkin Alexey <alex@centralhardware.ru>
2021-02-06 20:28:09 +0700

    fix interceptor

--- src/main/java/ru/centralhardware/asiec/inventory/FilterDeleteHibernateInterceptor.java
+++ src/main/java/ru/centralhardware/asiec/inventory/FilterDeleteHibernateInterceptor.java
@@ -2,24 +2,20 @@ package ru.centralhardware.asiec.inventory;
 
 import org.hibernate.EmptyInterceptor;
 import org.hibernate.type.Type;
-import org.springframework.stereotype.Component;
 import ru.centralhardware.asiec.inventory.Entity.Deletable;
 
 import java.io.Serializable;
 
-@Component
-public class CustomInterceptorImpl extends EmptyInterceptor {
+public class FilterDeleteHibernateInterceptor extends EmptyInterceptor {
 
     @Override
     public boolean onLoad(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
         if (entity instanceof Deletable){
             Deletable deletable = (Deletable) entity;
             if (deletable.isDeleted()){
-                return false;
-            } else {
-                return super.onLoad(entity, id, state, propertyNames, types);
+                throw new RuntimeException();
             }
         }
-        return false;
+        return super.onLoad(entity, id, state, propertyNames, types);
     }
 }
