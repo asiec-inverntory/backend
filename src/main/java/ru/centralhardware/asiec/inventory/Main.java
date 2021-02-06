ff46ee32c033e9f58b79a9d6d7697f7f6ec68d56
Fedechkin Alexey <alex@centralhardware.ru>
2021-02-06 21:19:03 +0700

    EnableJpaAuditing

--- src/main/java/ru/centralhardware/asiec/inventory/Main.java
+++ src/main/java/ru/centralhardware/asiec/inventory/Main.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan("ru.centralhardware.asiec.inventory")
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableAutoConfiguration
 @Configuration
 @SpringBootApplication()
+@EnableJpaAuditing
 public class Main {
 
     public static void main(String[] args) {
