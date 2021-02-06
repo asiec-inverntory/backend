43efab79e07ffbd6ce12b404d5ebf16e7257af60
Fedechkin Alexey <alex@centralhardware.ru>
2021-02-06 21:06:45 +0700

    fix cannot find symbol

--- src/main/java/ru/centralhardware/asiec/inventory/Dto/Create/CreateEquipmentDto.java
+++ src/main/java/ru/centralhardware/asiec/inventory/Dto/Create/CreateEquipmentDto.java
@@ -2,6 +2,7 @@ package ru.centralhardware.asiec.inventory.Dto.Create;
 
 public class CreateEquipmentDto {
 
+    public int id;
     public String name;
     public String inventory_code;
     public int room;
--- src/main/java/ru/centralhardware/asiec/inventory/Dto/Create/CreateUserDto.java
+++ src/main/java/ru/centralhardware/asiec/inventory/Dto/Create/CreateUserDto.java
@@ -4,6 +4,7 @@ import ru.centralhardware.asiec.inventory.Entity.Enum.Role;
 
 public class CreateUserDto {
 
+    public int id;
     public String username;
     public String password;
     public String name;
