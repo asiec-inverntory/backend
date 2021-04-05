3a41452e1cd6fabf160b47e187b368453c5e21cd
Fedechkin Alexey Borisovich <alex@centralhardware.ru>
2021-04-06 01:31:35 +0700

    start feeling test data

---
+++ src/main/resources/dev/db/migration/V1.3__add_test_data.sql
@@ -0,0 +1,27 @@
+INSERT INTO equipment (
+    name,
+    inventory_code,
+    room,
+    parent_equipment,
+    is_atomic,
+    appointment,
+    position,
+    responsible,
+) VALUES (
+    'Монитор',
+    4,
+
+)
+
+INSERT INTO room (
+    number,
+    flour,
+    description,
+    appointment,
+    building,
+    responsible,
+) VALUES (
+
+)
+
+
