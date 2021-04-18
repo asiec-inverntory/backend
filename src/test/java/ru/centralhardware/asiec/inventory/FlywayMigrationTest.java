a476e36bbaf7917047e1c9bafa60c2441c9610dd
Fedechkin Alexey Borisovich <alex@centralhardware.ru>
2021-04-18 15:34:05 +0700

    add dev migration locale to flyway test

--- src/test/java/ru/centralhardware/asiec/inventory/FlywayMigrationTest.java
+++ src/test/java/ru/centralhardware/asiec/inventory/FlywayMigrationTest.java
@@ -16,7 +16,8 @@ public class FlywayMigrationTest {
     @Test
     public void testMigration() {
         Flyway.configure()
-                .dataSource(postgreSQLContainer.getJdbcUrl(), postgreSQLContainer.getUsername(), postgreSQLContainer.getPassword())
+                .dataSource(postgreSQLContainer.getJdbcUrl(), postgreSQLContainer.getUsername(), postgreSQLContainer.getPassword()).
+                locations("classpath:/db/migration,classpath:/dev/db/migration")
                 .load()
                 .migrate();
     }
