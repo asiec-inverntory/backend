110b1ef2ed905dedb0cfc75f0787c1e00058bda1
Fedechkin Alexey Borisovich <alex@centralhardware.ru>
2021-04-18 15:05:36 +0700

    hash password on server

--- README.md
+++ README.md
@@ -5,6 +5,4 @@
 default user: 
 
 - username: admin
-- password: qXfkr9baKTQR4D (sha512: 8201c12212ab1aaa8acf4d2f5386939db503003eb43bac0dd7abd5d6080ebd65)
-
-test JWT token: eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY0Mzk4ODM1MywiaWF0IjoxNjEyNDUyMzUzfQ.zJuivEcKwklLqZkcMGHJ-n2ssfvpYq9iJtTU-CRYROafclIYwNAPrLdyz4WT2POpXc46Ld0COfXpL7LtANk-zw ее надо засунуть в куку с именем authorisation
+- password: 
--- src/main/java/ru/centralhardware/asiec/inventory/Security/WebSecurityConfig.java
+++ src/main/java/ru/centralhardware/asiec/inventory/Security/WebSecurityConfig.java
@@ -9,7 +9,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -30,8 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public static NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     /**
